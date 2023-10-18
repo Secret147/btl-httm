402bee3da1a36013c5c7f2a9462ccbb385c34768
Secret147 <chungnguyen2002xtc@gmail.com>
2023-10-18 14:30:21 +0700

    Initial commit

---
+++ logistic-be/src/main/java/com/httm/logisticbe/LogisticBeApplication.java
@@ -0,0 +1,13 @@
+package com.httm.logisticbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticBeApplication.class, args);
+	}
+
+}
---
+++ logistic-be/src/test/java/com/httm/logisticbe/LogisticBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.httm.logisticbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
